3b41254ade29b5876b5bce1baeaa46389556cc24
Ayub <ayubmja@icloud.com>
2024-06-10 13:29:21 -0400

    finished the menu page

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -3,7 +3,7 @@
     <x-nav/>
     <div class="px-10">
         <div class="container mx-auto border border-white/60 max-w-full py-4 mt-5">
-            <div class="grid grid-cols-3 text-center gap-4 px-2">
+            <div class="grid grid-cols-2 text-center gap-4 px-2">
                 <div class="border border-white/90 p-5">
                     <h1 class="mb-5">Chicken Salad</h1>
                     <div class="grid grid-cols-2">
@@ -11,7 +11,7 @@
                             <img src="{{Vite::asset('resources/images/menu1.png')}}" alt="Chicken Shop poster one">
                         </div>
                         <div class="m-auto">
-                            <h1>Listen nobody likes salad especially us. You are welcome.</h1>
+                            <h1>Listen nobody likes salad especially us. You are welcome. It is just chicken <span class="underline font-bold text-2xl text-red-700">nothing more and nothing less fam</span></h1>
                         </div>
 
                         <div class="mt-5">
@@ -27,36 +27,19 @@
                     <h1 class="mb-5">Chicken life</h1>
                     <div class="grid grid-cols-2">
                         <div>
-                            <h1>1</h1>
+                            <img src="{{Vite::asset('resources/images/chickencereal.webp')}}" alt="cereal but chicken cereal">
                         </div>
-                        <div>
-                            <h1>2</h1>
-                        </div>
-
-                        <div>
-                            <h1>3</h1>
-                        </div>
-                        <div>
-                            <h1>4</h1>
-                        </div>
-                    </div>
-                    <h1 class="mt-5">Don't like this go somewhere else fam. Don't trip dawg</h1>
-                </div>
-                <div class="border border-white/90 p-5">
-                    <h1 class="mb-5">Chicken Death</h1>
-                    <div class="grid grid-cols-2">
-                        <div>
-                            <h1>1</h1>
-                        </div>
-                        <div>
-                            <h1>2</h1>
+                        <div class="m-auto">
+                            <h1>
+                                One of our master pieces cereal but its all chicken top chefs only wish to know our secret. Just in time for breakfast special.
+                            </h1>
                         </div>
 
-                        <div>
-                            <h1>3</h1>
+                        <div class="mt-5">
+                            <img src="{{Vite::asset('resources/images/chickenlasagne.webp')}}" alt="Sad vegans eating chicken">
                         </div>
-                        <div>
-                            <h1>4</h1>
+                        <div class="m-auto">
+                            <h1>Chicken lasagne there is no pasta in this one boys 100% chicken we'll give you the recipe don't trip dawg we got you.</h1>
                         </div>
                     </div>
                     <h1 class="mt-5">Don't like this go somewhere else fam. Don't trip dawg</h1>
